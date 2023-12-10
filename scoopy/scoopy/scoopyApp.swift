eb55ba3af066a6b164138cda39f32a5b87e7f334
kris-randen <kris.randen@gmail.com>
2023-12-10 13:13:06 +0530

    Created the scoopy Xcode project.

---
+++ scoopy/scoopy/scoopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  scoopyApp.swift
+//  scoopy
+//
+//  Created by Krishnaswami Rajendren on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct scoopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
